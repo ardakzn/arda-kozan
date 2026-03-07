c5d4346ef142b056cae0a52e79ccb32e9e92f021
Arda Kozan <arda.kzn@gmail.com>
2026-03-07 15:59:57 +0300

    config changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/portfolio-arda/' : '/',
+  base: mode === 'production' ? '/arda-kozan/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
